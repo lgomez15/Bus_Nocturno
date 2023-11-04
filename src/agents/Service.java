9783b4e90b37235ca11a9f28e96a2307f51559ea
Nuno Escudero <nuescudero@usal.es>
2023-11-04 19:29:27 +0100

    clientV2 ServiceV1

--- src/agents/Service.java
+++ src/agents/Service.java
@@ -1,5 +1,45 @@
 package agents;
 
-public class Service {
+import behaviours.ServiceBehaviour;
+import jade.core.Agent; 
+import jade.domain.DFService; 
+import jade.domain.FIPAAgentManagement.DFAgentDescription; 
+import jade.domain.FIPAAgentManagement.ServiceDescription; 
+import jade.domain.*;
+
+public class Service extends Agent{
     
+    protected void setup()
+    {
+        /*REGISTRO SERVICIO */
+        /*1- Creamos el agente */
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        /*2- Descripción del servicio */
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("Servicio"); // Tipo de servicio.
+        sd.setName("Servicio"); // Nombre del servicio.
+        dfd.addServices(sd); // Añade el servicio a la descripción del agente.
+        /*Registramos elagente servicio */
+        try{
+            DFService.register(this, dfd);
+        }catch(FIPAException fe){
+            System.out.println("Error al registrar el servicio " + fe);
+        }
+
+
+        this.addBehaviour(new ServiceBehaviour());
+    }
+
+    protected void takeDown()
+    {
+        try{
+            DFService.deregister(this);
+        }catch(FIPAException fe){
+            System.out.println("Error al desregistrar el servicio " + fe);
+        }
+    }
+
+
+
 }
